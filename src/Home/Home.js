5ac514013752e6de1b789ebb741e8f78829a01d4
407 <407@academicos.cesde.edu.co>
2022-11-29 16:39:00 -0500

    se crea proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,60 @@
+import './Home.css'
+export function Home(){
+    return(
+        <>
+            <section>
+                <div class="banner">
+
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
+                            <h2>Crew Peligrosos</h2>
+                            <p>En Aranjuez hay un combo de peligrosos. Hay que tener cuidado porque para ellos no hay nada imposible en la vida y estan cambiando la forma de pensar en esta ciudad. Ellos son los Crew Peligrosos, un proyecto que nace en medio de la violencia, pero su reacción no es violenta. Es creativa. Transformadora.
+
+                            Precisamente, cuando la violencia se agudiza, cuando a su parche lo atacan o cuando en su barrio siguen cayendo más y más personas, ellos en lugar de esconderse, salen a la calle a bailar, a cantarle a la vida, a enseñarle a las nuevas generaciones cual es la salida y que la propuesta está en el arte, la cultura. En el poder de la música como una forma de atacar y hacer resistencia a la violencia.
+
+                            El proyecto de Crew Peligrosos nació como una alternativa artística para la violencia en la comuna 4 de Medellín, liderada por Henry Arteaga 'JKE' (Jeque) MC. Se dieron a conocer en emisoras con su tema 'Medayork' y ahora buscan conquistar el territorio nacional con 'Mera Vuelta'. La canción fue producida por Toy Selectah y es un adelanto de los que será su primer álbum de estudio.</p>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotydseg.appspot.com/o/peligrosos.jpg?alt=media&token=ccc70ee8-118d-4c89-a94e-f6b978f2a57a" alt=""></img>
+
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <i class="bi bi-vinyl-fill fs-1">
+                                <div>
+                                    <h1>Medellín</h1>
+                                    <h2>dic 2</h2>
+                                </div>
+                            </i>
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                        </div>
+                        <div>
+                            
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotydseg.appspot.com/o/crew1.jpg?alt=media&token=a32b2df6-db4d-4e97-9e30-5e0cf1420910" alt="" class="img-fluid w-100"/>
+                        </div>
+                        <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotydseg.appspot.com/o/crew3.jpg?alt=media&token=297bc6bc-36a3-4e26-b45a-fdb445ac3bf9" alt="" class="img-fluid w-100"/>
+                        </div>
+                        <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotydseg.appspot.com/o/crew4.jpg?alt=media&token=1424fc9a-95c5-4bf0-821a-c7b2afe68c78" alt="" class="img-fluid w-100"/>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
